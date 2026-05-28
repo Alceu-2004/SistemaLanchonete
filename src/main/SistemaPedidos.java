daad3bf14a9886305b30ad76c81da179959a79a1
Pedro Sias <omardotaxi@gmail.com>
2026-05-28 09:20:11 -0300

    substituir números mágicos por constantes

--- src/main/SistemaPedidos.java
+++ src/main/SistemaPedidos.java
@@ -1,23 +1,28 @@
 public class SistemaPedidos {
 
+    private static final double PRECO_HAMBURGUER = 15;
+    private static final double PRECO_PIZZA = 20;
+    private static final double PRECO_COMBO = 30;
+    private static final double DESCONTO_VIP = 0.10;
+
     public void processarPedido(Pedido pedido) {
 
         double total = 0;
 
         if (pedido.produto.tipo == 1) {
-            total = pedido.produto.quantidade * 15;
+            total = pedido.produto.quantidade * PRECO_HAMBURGUER;
         }
 
         if (pedido.produto.tipo == 2) {
-            total = pedido.produto.quantidade * 20;
+            total = pedido.produto.quantidade * PRECO_PIZZA;
         }
 
         if (pedido.produto.tipo == 3) {
-            total = pedido.produto.quantidade * 30;
+            total = pedido.produto.quantidade * PRECO_COMBO;
         }
 
         if (pedido.cliente.vip == true) {
-            total = total - (total * 0.10);
+            total = total - (total * DESCONTO_VIP);
         }
 
         System.out.println("Cliente: " + pedido.cliente.nome);
